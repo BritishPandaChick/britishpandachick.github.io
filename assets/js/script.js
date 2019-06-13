0c095ad00ea40d1d0a6d5162553e300e6f9d3081
BritishPandaChick <sbartley1080@gmail.com>
2019-06-12 20:03:17 -0400

    Add JS functions for buttons. Add new sections to about and portfolio pages. Add new styles to style sheets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
-//console.log("Hello World");
+//BritishPandaChick Code blog button opens blog when clicked
 function openBPCCodes() {
   window.open("http://britishpandachickcodes.blogspot.com", "_blank");
+}
+
+//Needlework Kits blog button opens blog when clicked
+function openNeedleworkKits() {
+  window.open("http://needleworkkits.blogspot.com", "_blank");
+}
+
+//Project 1 button opens to github repository for project
+function openProjectOne() {
+  window.open("https://github.com/BritishPandaChick/tribute-page", "_blank");
+}
+
+//Project 2 button opens to github repository for project
+function openProjectTwo() {
+  window.open("https://github.com/BritishPandaChick/magic-eight-ball", "_blank");
+}
+
+//Project 3 button opens to github repository for project
+function openProjectThree() {
+  window.open("https://github.com/BritishPandaChick/lolcatclock", "_blank");
 }
