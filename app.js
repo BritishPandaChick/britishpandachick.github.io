78e2ab3bfe48b0f446ee47dc72dcb4e9e447a90e
BritishPandaChick <sbartley1080@gmail.com>
2020-01-01 17:40:38 -0500

    Change port information for app.listen

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Portfolio is connected");
 });
