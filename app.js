81d7dc1e943d21990ed8df4073d0c38d5d797294
BritishPandaChick <sbartley1080@gmail.com>
2020-01-02 19:40:14 -0500

    Comment out code in ejs files. Delete portfolio folder and index.html file. Start adding app.get for each of the EJS pages

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Portfolio is connected");
+app.get("/", function(req, res){
+  res.render("landing");
 });
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.get("/portfolio", function(req, res){
+  res.render("portfolio");
+})
+
+
+app.listen(port, () => console.log("YelpCamp Server has started"));
