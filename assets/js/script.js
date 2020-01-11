4c9866525cca240d4a40af5db1e9c9aa50ec9eea
BritishPandaChick <sbartley1080@gmail.com>
2020-01-10 19:28:05 -0500

    deleted images from img folder. add new image for about page. Move awards and speaking sections from portfolio to about page. Replace about me text with placeholder text

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,16 @@ function openNeedleworkKits() {
   window.open("http://needleworkkits.blogspot.com", "_blank");
 }
 
+//Disney Codes Challenge button opens article
+function openDisney(){
+  window.open("https://dev.to/theoriginalbpc/everything-you-need-to-know-about-disney-codes-challenge-3ojb");
+}
+
+//Elpha button opens to Elpha home page
+function openElpha(){
+  window.open("https://elpha.com/");
+}
+
 //Project 1 button opens to github repository for project
 function openProjectOne() {
   window.open("https://github.com/BritishPandaChick/tribute-page", "_blank");
