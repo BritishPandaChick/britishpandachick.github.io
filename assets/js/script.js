5976d6067f45476eb070ca97d3ddb03f96ff5855
BritishPandaChick <sbartley1080@gmail.com>
2020-09-21 22:55:23 -0400

    Add project folders and files. Update page content for home, about, and portfolio pages. Delete panda image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-/*About Page */
-//BritishPandaChick Code blog button opens blog when clicked
-function openBritishCodes() {
-  window.open("http://britishpandachickcodes.blogspot.com", "_blank");
-}
-
-//Needlework Kits blog button opens blog when clicked
-function openNeedleKits() {
-  window.open("http://needleworkkits.blogspot.com", "_blank");
-}
-
 //Disney Codes Challenge button opens article
 function openDisneyCodes(){
   window.open("https://dev.to/theoriginalbpc/everything-you-need-to-know-about-disney-codes-challenge-3ojb", "_blank");
@@ -19,33 +8,3 @@ function openElpha(){
   window.open("https://elpha.com/", "_blank");
 }
 
-/* Portfolio Page */
-//Opens web app for Yelpcamp
-function openYelpCamp(){
-  window.open("https://github.com/BritishPandaChick/yelpcamp", "_blank");
-}
-
-//Opens Github repo for author website
-function openAuthorSite(){
-  window.open("https://github.com/BritishPandaChick/sean-author-website", "_blank");
-}
-
-//Open pen for Tribute Page
-function openTributePage(){
-  window.open("https://codepen.io/BritishPandaChick/full/BKBLOv", "_blank");
-}
-
-//Open Github repo for Magic 8 Ball project
-function openMagicBall(){
-  window.open("https://github.com/BritishPandaChick/magic-eight-ball", "_blank");
-}
-
-//Open Github repo for election map project
-function openElectionMap(){
-  window.open("https://github.com/BritishPandaChick/election-map", "_blank");
-}
-
-//Open Github repo for LOL Cat Clock Project
-function openCatClock(){
-  window.open("https://github.com/BritishPandaChick/lolcatclock", "_blank");
-}
