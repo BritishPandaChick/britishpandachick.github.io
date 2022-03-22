2e6bb0dc7fbdccee6ad666bc201b458c6f6f2b1d
BritishPandaChick <sbartley1080@gmail.com>
2022-03-22 16:50:39 -0400

    Update side-projects section. Remove openElpha function in script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,3 @@
 function openDisneyCodes(){
   window.open("https://dev.to/theoriginalbpc/everything-you-need-to-know-about-disney-codes-challenge-3ojb", "_blank");
 }
-
-//Elpha button opens to Elpha home page
-function openElpha(){
-  window.open("https://elpha.com/", "_blank");
-}
-
