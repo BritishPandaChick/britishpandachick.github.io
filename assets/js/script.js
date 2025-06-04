9d350567fb77e6be0ecaf4921f5529bac0a616a0
britishpandachick <sbartley1080@gmail.com>
2025-06-04 19:43:24 -0400

    Start updating mobile.css file. Fix JS code so navbar works on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
-const header = document.querySelector('.navbar');
 
-window.onscroll = function() {
-  var top = window.scrollY;
-  if(top >= 100) {
-    header.classList.add('navbarDark');
-  } else {
-    header.classList.remove('navbarDark');
-  }
-}
+document.addEventListener('DOMContentLoaded', () => {
 
-//Disney Codes Challenge button opens article
-function openDisneyCodes(){
-  window.open("https://dev.to/theoriginalbpc/everything-you-need-to-know-about-disney-codes-challenge-3ojb", "_blank");
-}
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
